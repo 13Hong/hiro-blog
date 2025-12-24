0c1c3e6a201a0d6f0b406e06f48df88c0663fc16
hiro <571507098@qq.com>
2025-12-24 21:11:28 +0800

    refactor(photo):引言文字改为打字机效果

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export default {
   我是一名「前端开发工程师」`,
   friend:'Maybe we can make friends',
   live:'𝑇ℎ𝑒𝑟𝑒 𝑖𝑠 𝑜𝑛𝑙𝑦 𝑜𝑛𝑒 ℎ𝑒𝑟𝑜𝑖𝑠𝑚 𝑖𝑛 𝑡ℎ𝑒 𝑤𝑜𝑟𝑙𝑑: 𝑡𝑜 𝑠𝑒𝑒 𝑡ℎ𝑒 𝑤𝑜𝑟𝑙𝑑 𝑎𝑠 𝑖𝑡 𝑖𝑠, 𝑎𝑛𝑑 𝑡𝑜 𝑙𝑜𝑣𝑒 𝑖𝑡.',
-  picture:'「优游处其境，岁月忽其期」',
+  picture:'「⌚️时间向前，📸照片替我停留」',
   navs: [
     {
       key: 'home',
