651a7edcb0962d03fe1565c03d747938851ce636
hiro <571507098@qq.com>
2025-12-26 21:07:54 +0800

    refactor(photo):更新

--- src/components/vue/Photoalbum.vue
+++ src/components/vue/Photoalbum.vue
@@ -42,6 +42,141 @@ const data = ref({
       },
       href: "https://img.hiro7.info/images/zhouye4.jpg",
     },
+    {
+      label: "睡觉猫猫🐱💤",
+      time: "2025/10/12",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/202510123.jpg",
+    },
+    {
+      label: "睡觉猫猫🐱💤",
+      time: "2025/10/12",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/202510121.jpg",
+    },
+    {
+      label: "高速猫猫🐱",
+      time: "2025/10/12",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20251012.jpg",
+    },
+    {
+      label: "露营喝茶🍵",
+      time: "2024/10/11",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20251011.jpg",
+    },
+    {
+      label: "日落🌄",
+      time: "2025/10/07",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20251007.jpg",
+    },
+    {
+      label: "看门狗🐶",
+      time: "2024/07/13",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20240713.jpg",
+    },
+    {
+      label: "家乡猫🐱",
+      time: "2024/06/08",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/202406081.jpg",
+    },
+    {
+      label: "家乡猫🐱",
+      time: "2024/06/08",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20240608.jpg",
+    },
+    {
+      label: "温馨一家猫🐱",
+      time: "2024/05/04",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20240504.jpg",
+    },
+    {
+      label: "玉龙雪山🏔️",
+      time: "2024/02/11",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/202402111.jpg",
+    },
+    {
+      label: "玉龙雪山🏔️",
+      time: "2024/02/11",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20240211.jpg",
+    },
+    {
+      label: "灰背鸫",
+      time: "2024/02/10",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20240210.jpg",
+    },
+    {
+      label: "日出🌄",
+      time: "2024/02/08",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/202402081.jpg",
+    },
+    {
+      label: "日出🌄",
+      time: "2024/02/08",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20240208.jpg",
+    },
+    {
+      label: "红嘴鸥",
+      time: "2024/02/05",
+      size: {
+        width: 0,
+        height: 0,
+      },
+      href: "https://img.hiro7.info/images/20240205.jpg",
+    },
   ],
 });
 
